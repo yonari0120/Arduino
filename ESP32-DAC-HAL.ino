e79ef3c71524bd5bdb86842b555f2c859118f7e1
yonari0120 <71751812+yonari0120@users.noreply.github.com>
2022-03-17 21:09:18 +0800

    Add files via upload

---
+++ ESP32-DAC-HAL.ino
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+#include <esp32-hal-dac.h>//DAC功能引用該庫，可以不進行#include<>引用
+
+//查看源碼後得到，DAC引腳只能是這兩個引腳
+#define LED1 25
+#define LED2 26
+ 
+void setup(){
+  //長時候發現，DAC的IO口也可以不進行初始化
+  pinMode(LED1,OUTPUT);
+  pinMode(LED2,OUTPUT);
+}
+ 
+void loop()
+{
+  for (int dutyCycle = 0; dutyCycle <= 255; dutyCycle = dutyCycle + 1)
+  {
+    dacWrite(LED1, dutyCycle);
+    dacWrite(LED2, 255 - dutyCycle);
+    delay(5);
+  }
+ 
+  for (int dutyCycle = 255; dutyCycle >= 0; dutyCycle = dutyCycle - 1)
+  {
+    dacWrite(LED1, dutyCycle);
+    dacWrite(LED2, 255 - dutyCycle);
+    delay(5);
+  }
+}
